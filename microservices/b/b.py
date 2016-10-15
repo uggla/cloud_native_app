58d7842a1b74da0f501f5ae4b9301e4a5f2b2e0a
Uggla <uggla@free.fr>
2016-10-15 16:39:27 +0200

    Add draft b service

---
+++ microservices/b/b.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+Service B trigger service W in order to define the user price, then update
+redis and send a mail via rabbitmq.
+"""
+
+import logging
+from logging.handlers import RotatingFileHandler
+import pprint
+import os
+import sys
+from flask import Flask
+from flask import jsonify
+from flask import request
+import requests
+import config
+
+# Initialise Flask
+app = Flask(__name__)
+app.debug = True
+
+# Affect app logger to a global variable so logger can be used elsewhere.
+config.logger = app.logger
+
+
+@app.route("/user/<id>")
+def api_play(id):
+    """Retrieve data for user <id>"""
+    config.logger.info("*** Start processing id %s ***", id)
+
+    data = {id: "not_played"}
+
+    resp = jsonify(data)
+    resp.status_code = 200
+    config.logger.info("*** End processing id %s ***", id)
+    add_headers(resp)
+    return resp
+
+
+@app.route("/shutdown", methods=["POST"])
+def shutdown():
+    """Shutdown server"""
+    shutdown_server()
+    config.logger.info("Stopping %s...", config.b.NAME)
+    return "Server shutting down..."
+
+
+@app.route("/", methods=["GET"])
+def api_root():
+    """Root url, provide service name and version"""
+    data = {
+        "Service": config.b.NAME,
+        "Version": config.b.VERSION
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    resp.headers["AuthorSite"] = "https://github.com/uggla/openstack_lab"
+
+    add_headers(resp)
+    return resp
+
+
+def shutdown_server():
+    """shutdown server"""
+    func = request.environ.get("werkzeug.server.shutdown")
+    if func is None:
+        raise RuntimeError("Not running with the Werkzeug Server")
+    func()
+
+
+def configure_logger(logger, logfile):
+    """Configure logger"""
+    formatter = logging.Formatter(
+        "%(asctime)s :: %(levelname)s :: %(message)s")
+    file_handler = RotatingFileHandler(logfile, "a", 1000000, 1)
+
+    # Add logger to file
+    if (config.b.conf_file.get_b_debug().title() == 'True'):
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+
+def add_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
+
+
+if __name__ == "__main__":
+    # Vars
+    app_logfile = "b.log"
+
+    # Change diretory to script one
+    try:
+        os.chdir(os.path.dirname(sys.argv[0]))
+    except FileNotFoundError:
+        pass
+
+    # Define a PrettyPrinter for debugging.
+    pp = pprint.PrettyPrinter(indent=4)
+
+    # Initialise apps
+    config.initialise_b()
+
+    # Configure Flask logger
+    configure_logger(app.logger, app_logfile)
+
+    config.logger.info("Starting %s", config.b.NAME)
+    app.run(port=int(config.b.conf_file.get_b_port()))
---
+++ microservices/b/config.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+
+import configparser
+
+# Initialise global variable
+logger = None
+b = None
+
+
+def initialise_b():
+    """Define s global object so it can be called from anywhere."""
+    global b
+    b = B()
+
+
+class B(object):
+    def __init__(self):
+        self.NAME = "Microservice b"
+        self.VERSION = "0.1"
+        #
+        # Configuration file
+        self.conf_file = bConfiguration("b.conf")
+
+
+class bConfiguration(object):
+
+    def __init__(self, configuration_file):
+        self.config = configparser.ConfigParser(allow_no_value=True)
+        self.config.read(configuration_file)
+
+    def get_b_port(self):
+        return self.config.get("b", "port")
+
+    def get_b_debug(self):
+        return self.config.get("b", "debug")
+
