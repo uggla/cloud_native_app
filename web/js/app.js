27dcbc391d6205664af6acdd50c8a585d7d4bc5b
Uggla <uggla@free.fr>
2016-10-15 16:38:30 +0200

    Improve html page

--- web/js/app.js
+++ web/js/app.js
@@ -4,33 +4,72 @@
  */
 $( document ).ready(function() {
 	console.log( "Document ready !" );
-	console.log($.urlParam('id'));
-	srvI = new ServiceI();
-	srvI.ajax("http://localhost:8080/");
+	id = $.urlParam("id");
+	console.log("id=" + id);
+	if (id=='' || id==null){
+		$("body").html("Please provide a user id !");
+		$("body").css({"opacity":"100"});
+	}
+	else{	
+		$("body").css({"opacity":"100"});
+		srvI = new Service();
+		srvI.ajax(I, "http://localhost:8080/user/" + id);
+	}
 });
 
 
-function ServiceI(){
-	/* Object to manage ServiceI api */
-	this.ajax = function(url){
+function Service(){
+	/* Object to manage Service api */
+	this.ajax = function(service_name, url){
 		var response = $.ajax({
 			url: url,
 			type: "GET",
 			crossDomain:true,
 			success: function (response) {
 				console.log(response);
-				console.log(response.Service);
+				service_name("ok", response)
 			},
 			error: function (xhr, status, ajaxerror) {
 				console.log(status);
 				console.log(ajaxerror);
-				throw status + " connecting to " + url;
+				//throw status + " connecting to " + url;
+				service_name("ko")
 			}
 		});
 	}
 }
 
+function I(status, response)
+{
+	if (status=="ok"){
+		// Craft the answer.
+		if (response.id=="Not found"){
+			$("#srvIstatus").html("User id not found.")
+		}
+		else{
+			var answer = '';
+			answer += '<table id="userdata">';
+			answer += '<tr>';
+			answer += '<td>id</td><td>' + response.id + '</td>';
+			answer += '</tr>';
+			answer += '<tr>';
+			answer += '<td>firstname</td><td>' + response.firstname + '</td>';
+			answer += '</tr>';
+			answer += '<tr>';
+			answer += '<td>lastname</td><td>' + response.lastname + '</td>';
+			answer += '</tr>';
+			answer += '<tr>';
+			answer += '<td>email</td><td>' + response.email + '</td>';
+			answer += '</tr>';
+			answer += '</table>';
+			$("#srvIstatus").html(answer)
+		}
+	}
+	else{
+		$("#srvIstatus").html("Service <b>i</b> is currently not available.")
 
+	}
+}
 
 $.urlParam = function(name){
     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
