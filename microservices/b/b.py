8ea202be1869b41e12d3d2fc91b7ebd60679dca7
Uggla <uggla@free.fr>
2016-10-17 13:45:33 +0200

    Service b use multiprocessing to allow parallelisation

--- microservices/b/b.py
+++ microservices/b/b.py
@@ -114,4 +114,4 @@ if __name__ == "__main__":
     configure_logger(app.logger, app_logfile)
 
     config.logger.info("Starting %s", config.b.NAME)
-    app.run(port=int(config.b.conf_file.get_b_port()))
+    app.run(port=int(config.b.conf_file.get_b_port()), processes=4)
