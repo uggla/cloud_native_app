7c1854888ee056fb4da65576ec9f501f77daa74a
Uggla <uggla@free.fr>
2016-10-27 21:15:29 +0200

    Add config file + logger

---
+++ microservices/w1/config.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+
+import configparser
+
+
+class Configuration(object):
+
+    def __init__(self, configuration_file):
+        self.config = configparser.ConfigParser(allow_no_value=True)
+        self.config.read(configuration_file)
+
+    def get_w1_rabbithost(self):
+        return self.config.get("w1", "rabbithost")
+
+    def get_w1_rabbitlogin(self):
+        return self.config.get("w1", "rabbitlogin")
+
+    def get_w1_rabbitpassword(self):
+        return self.config.get("w1", "rabbitpassword")
+
+    def get_w1_debug(self):
+        return self.config.get("w1", "debug")
+
+    def get_w1_redishost(self):
+        return self.config.get("w1", "redishost")
+
+    def get_w1_os_parameters(self):
+        parameters = {}
+        parameters.update(
+            {"os_authurl": self.config.get("w1", "os_authurl")})
+        parameters.update(
+            {"os_auth_version": self.config.get("w1", "os_auth_version")})
+        parameters.update(
+            {"os_user": self.config.get("w1", "os_user")})
+        parameters.update(
+            {"os_key": self.config.get("w1", "os_key")})
+        parameters.update(
+            {"os_tenant_name": self.config.get("w1", "os_tenant_name")})
+        return parameters
--- microservices/w1/w1.py
+++ microservices/w1/w1.py
@@ -1,18 +1,84 @@
 #!/usr/bin/env python3
 
 # Worker 1 called by service B. This worker listen to amqp messages and write
-# to redis that the user played.
+# to redis that the user played, and push the image price to swift.
 
+import os
+import sys
 import io
 import datetime
 import json
+import logging
+from logging.handlers import RotatingFileHandler
 import pika
 import redis
 import swiftclient
+import config
 
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+def initialize_logger(LOGFILE,
+                      CONSOLE_LOGGER_LEVEL,
+                      FILE_LOGGER_LEVEL,
+                      logger_name=None):
+    '''Initialize a global logger to track application behaviour
+
+    :param logfile: Log filename
+    :type logfile: str
+    :param screen_logger_level: Console log level
+                                (logging.DEBUG, logging.ERROR, ..) or nolog
+    :type screen_logger_level: logging constant or string
+    :param file_logger_level: File log level
+    :type file_logger_level: logging constant
+    :returns:  logging object
+
+    '''
+
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        '%(asctime)s :: %(levelname)s :: %(message)s')
+
+    try:
+        file_handler = RotatingFileHandler(
+            os.path.expandvars(LOGFILE), 'a', 1000000, 1)
+    except IOError:
+        print('ERROR: {} does not exist or is not writeable.\n'.format(
+            LOGFILE))
+        print('       Try to create directory {}'.format(os.path.dirname(
+            LOGFILE)))
+        print('       using: mkdir -p {}'.format(os.path.dirname(
+            LOGFILE)))
+        sys.exit(1)
+
+    # First logger to file
+    file_handler.setLevel(FILE_LOGGER_LEVEL)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    # Second logger to console
+    if CONSOLE_LOGGER_LEVEL != "nolog":
+        steam_handler = logging.StreamHandler()
+        steam_handler.setLevel(CONSOLE_LOGGER_LEVEL)
+        logger.addHandler(steam_handler)
+    return logger
+
+########################################
+# Main
+########################################
+
+logger = initialize_logger("w1.log", "nolog", logging.DEBUG)
+logger.info("*** Starting worker w1 ***")
+conf = config.Configuration("w1.conf")
+
+if conf.get_w1_rabbithost() == 'localhost':
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=conf.get_w1_rabbithost()))
+else:
+    credentials = pika.PlainCredentials(conf.get_w1_rabbitlogin(),
+                                        conf.get_w1_rabbitpassword())
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(credentials=credentials,
+                                  host=conf.get_w1_rabbithost()))
 
 channel = connection.channel()
 
@@ -29,20 +95,21 @@ print('Worker w1 running waiting for service b messages. To exit press CTRL+C')
 
 
 def callback(ch, method, properties, body):
-    print(" [x] %r" % body)
+    logger.debug(" [x] %r" % body)
     timestamp = datetime.datetime.now()
 
     # Write to redis
     data = json.loads(body.decode("utf-8"))
-    r = redis.Redis("localhost")
+    r = redis.Redis(conf.get_w1_redishost())
     r.set(data["id"], timestamp.strftime("%c"))
 
     # Write to swift
-    _authurl = 'http://10.3.222.89:5000/v2.0/'
-    _auth_version = '2'
-    _user = 'admin'
-    _key = 'password'
-    _tenant_name = 'demo'
+    os_parameters = conf.get_w1_os_parameters()
+    _authurl = os_parameters["os_authurl"]
+    _auth_version = os_parameters["os_auth_version"]
+    _user = os_parameters["os_user"]
+    _key = os_parameters["os_key"]
+    _tenant_name = os_parameters["os_tenant_name"]
 
     conn = swiftclient.Connection(
         authurl=_authurl,
@@ -65,7 +132,7 @@ def callback(ch, method, properties, body):
 
     content = io.BytesIO(data["img"].encode())
     filename = data["id"] + ".txt"
-    print("Filename : {}".format(filename))
+    logger.debug("Filename : {}".format(filename))
     conn.put_object(
         container_name,
         filename,
@@ -73,11 +140,12 @@ def callback(ch, method, properties, body):
         content_type='text/plain'
     )
 
-
     # Acknowledge message
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 channel.basic_consume(callback,
                       queue='redis')
-
-channel.start_consuming()
+try:
+    channel.start_consuming()
+except KeyboardInterrupt:
+    logger.info("*** Stopping worker w1 ***")
