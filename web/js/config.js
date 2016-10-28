bec7047974adde37d650ac1315af5abda76632d1
Uggla <uggla@free.fr>
2016-10-28 20:20:22 +0200

    Add config.js to define urls

---
+++ web/js/config.js
@@ -0,0 +1,9 @@
+/**
+ * Set service url
+ *
+ */
+
+var srvIurl = "http://localhost:8080/user/"
+var srvSurl = "http://localhost:8081/user/"
+var srvBurl = "http://localhost:8082/user/"
+var srvPurl = "http://localhost:8083/user/"
