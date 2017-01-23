fe31bb2772c49a4aea2b2ef4d485a1d447cc93b1
Bruno Cornec <bruno@victoria.frmug.org>
2017-01-23 01:57:42 +0100

    Add the missing s service to the list

--- docker_services.sh
+++ docker_services.sh
@@ -40,7 +40,7 @@ if [ _"$svc" = _"" ]; then
 fi
 
 # Build images if not done yet
-for a in web i b p w w1 w2 db; do
+for a in web i b p s w w1 w2 db; do
 	img=`docker images | grep -E "^cloudnativeapp_$a "`
 	if [ _"$img" = _"" ]; then
 		docker-compose up -d
@@ -48,7 +48,7 @@ for a in web i b p w w1 w2 db; do
 done
 
 # Push images in Registry if not done yet
-for a in web i b p w w1 w2 db; do
+for a in web i b p s w w1 w2 db; do
 	img=`docker images | grep -E "^$REGISTRY/cloudnativeapp_$a "`
 	if [ _"$img" = _"" ]; then
 		docker tag cloudnativeapp_$a $REGISTRY/cloudnativeapp_$a
@@ -57,13 +57,13 @@ for a in web i b p w w1 w2 db; do
 done
 
 # Launch services - Should be replaced by docker-compose v3 once available
-for a in web i b p w w1 w2 db; do
+for a in web i b p s w w1 w2 db; do
 	if [ $a = "web" ]; then
 		OPT="--publish 80:80"
 	elif [ $a = "w2" ]; then
 		OPT="--env W2_APIKEY=blakey --env W2_TO=machin@bidule.com --env W2_DOMAIN=domain"
 	elif [ $a = "db" ]; then
-    	OPT="--env MYSQL_ROOT_PASSWORD=toto --env MYSQL_DATABASE=prestashop --env MYSQL_USER=prestashop --env MYSQL_PASSWORD=prestashop1234" 
+		OPT="--env MYSQL_ROOT_PASSWORD=toto --env MYSQL_DATABASE=prestashop --env MYSQL_USER=prestashop --env MYSQL_PASSWORD=prestashop1234" 
 	else
 		OPT=""
 	fi
