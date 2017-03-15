eda2961d118fa1860082027d8defee0e3f35314c
Uggla <uggla@free.fr>
2017-03-15 17:36:59 +0100

    Move to composer V3. Update to work with docker 1.13+

--- docker_services.sh
+++ docker_services.sh
@@ -8,8 +8,19 @@
 REGISTRY=lab7-2.labossi.hpintelco.org:5500
 KEYSTONE=labossi.hpintelco.org
 WEB=`hostname`
+MYSQL_ROOT_PASSWORD=toto
+MYSQL_DATABASE=prestashop
+MYSQL_USER=prestashop
+MYSQL_PASSWORD=prestashop1234
+W2_APIKEY=blakey
+W2_TO=machin@bidule.com
+W2_DOMAIN=domain
 
-# Patch the javascript for the internal URLs to use 
+
+# Patch docker-compose-v3.yaml to pass our variables
+sed -i "s/##MYSQL_ROOT_PASSWORD##/$MYSQL_ROOT_PASSWORD/g" docker-compose-v3.yml
+
+# Patch the javascript for the internal URLs to use
 # the swarm leader as an entry point for internal micro-services
 sed "s/localhost/$WEB/" `dirname $0`/web/templates/config.js.docker
 
@@ -25,35 +36,17 @@ else
 	ENVOPT="-e HOST=$(docker-machine ls | head -2 | grep -v NAME | awk '{print $5}' | sed 's#tcp://##' | sed 's#:2376##') -e PORT=8080"
 fi
 
-docker ps | grep visualizer > /dev/null 2>&1 
+docker ps | grep visualizer > /dev/null 2>&1
 if [ $? -ne 0 ]; then
+    docker ps -a | grep visualizer && docker rm visualizer
     docker run -it -d -p 8080:8080 $ENVOPT -v /var/run/docker.sock:/var/run/docker.sock --name visualizer  manomarks/visualizer
-fi
-
-# Check if the overlay network is available
-docker network list | grep -E " cnalan " > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-    docker network create --driver overlay cnalan
-fi
-
-svc=`docker service ls | grep -E " rabbit "`
-if [ _"$svc" = _"" ]; then
-	docker service create --name rabbit --publish 15672:15672 \
-    --env RABBITMQ_DEFAULT_USER=stackrabbit \
-    --env RABBITMQ_DEFAULT_PASS=password \
-    --network cnalan rabbitmq:3-management
-fi
-
-svc=`docker service ls | grep -E " redis "`
-if [ _"$svc" = _"" ]; then
-	docker service create --name redis --network cnalan redis
 fi
 
 # Build images if not done yet
 for a in web i b p s w w1 w2 db; do
 	img=`docker images | grep -E "^cloudnativeapp_$a "`
 	if [ _"$img" = _"" ]; then
-		docker-compose up -d
+		docker-compose build
 	fi
 done
 
@@ -67,21 +60,23 @@ for a in web i b p s w w1 w2 db; do
 done
 
 # Launch services - Should be replaced by docker-compose v3 once available
-for a in web i b p s w w1 w2 db; do
-	if [ $a = "web" ]; then
-		OPT="--publish 80:80"
-	elif [ $a = "w2" ]; then
-		OPT="--env W2_APIKEY=blakey --env W2_TO=machin@bidule.com --env W2_DOMAIN=domain"
-	elif [ $a = "db" ]; then
-		OPT="--env MYSQL_ROOT_PASSWORD=toto --env MYSQL_DATABASE=prestashop --env MYSQL_USER=prestashop --env MYSQL_PASSWORD=prestashop1234" 
-	else
-		OPT=""
-	fi
+#for a in web i b p s w w1 w2 db; do
+#	if [ $a = "web" ]; then
+#		OPT="--publish 80:80"
+#	elif [ $a = "w2" ]; then
+#		OPT="--env W2_APIKEY=blakey --env W2_TO=machin@bidule.com --env W2_DOMAIN=domain"
+#	elif [ $a = "db" ]; then
+#		OPT="--env MYSQL_ROOT_PASSWORD=toto --env MYSQL_DATABASE=prestashop --env MYSQL_USER=prestashop --env MYSQL_PASSWORD=prestashop1234" 
+#	else
+#		OPT=""
+#	fi
+#
+#	svc=`docker service ls | grep -E " $a "`
+#	if [ _"$svc" = _"" ]; then
+#		docker service create --name $a --network cnalan $OPT $REGISTRY/cloudnativeapp_$a
+#	fi
+#done
 
-	svc=`docker service ls | grep -E " $a "`
-	if [ _"$svc" = _"" ]; then
-		docker service create --name $a --network cnalan $OPT $REGISTRY/cloudnativeapp_$a
-	fi
-done
-
+docker stack deploy -c docker-compose-v3.yml cna
+sleep 5
 docker service ls
--- docker_services_rm.sh
+++ docker_services_rm.sh
@@ -5,32 +5,34 @@
 #
 # You need to provide your Registry address here:
 #REGISTRY=uggla
-REGISTRY=lab7-2.labossi.hpintelco.org:5500
-KEYSTONE=labossi.hpintelco.org
-WEB=`hostname`
-svc=`docker service ls | grep -E " rabbit "`
-if [ _"$svc" != _"" ]; then
-	docker service rm rabbit
-fi
+#REGISTRY=lab7-2.labossi.hpintelco.org:5500
+#KEYSTONE=labossi.hpintelco.org
+#WEB=`hostname`
+#svc=`docker service ls | grep -E " rabbit "`
+#if [ _"$svc" != _"" ]; then
+#	docker service rm rabbit
+#fi
+#
+#svc=`docker service ls | grep -E " redis "`
+#if [ _"$svc" != _"" ]; then
+#	docker service rm redis
+#fi
+#
+## Launch services - Should be replaced by docker-compose v3 once available
+#for a in web i b p s w w1 w2 db; do
+#	svc=`docker service ls | grep -E " $a "`
+#	if [ _"$svc" != _"" ]; then
+#		docker service rm $a
+#	fi
+#done
+#
+#
+## Check if the overlay network is available
+#docker network list | grep -E " cnalan " > /dev/null 2>&1
+#if [ $? -eq 0 ]; then
+#    docker network rm cnalan
+#fi
 
-svc=`docker service ls | grep -E " redis "`
-if [ _"$svc" != _"" ]; then
-	docker service rm redis
-fi
-
-# Launch services - Should be replaced by docker-compose v3 once available
-for a in web i b p s w w1 w2 db; do
-	svc=`docker service ls | grep -E " $a "`
-	if [ _"$svc" != _"" ]; then
-		docker service rm $a
-	fi
-done
-
-
-# Check if the overlay network is available
-docker network list | grep -E " cnalan " > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-    docker network rm cnalan
-fi
-
+docker stack rm cna
+sleep 5
 docker service ls
