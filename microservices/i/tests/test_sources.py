83d2b79ef9ed556fb827c6b3cf916dc7a9da9adf
Uggla <uggla@free.fr>
2017-03-21 15:28:59 +0100

    Run test from anywhere in the tree

--- microservices/i/tests/test_sources.py
+++ microservices/i/tests/test_sources.py
@@ -1,11 +1,17 @@
 # coding=utf-8
 
 # This is a simple unitary test to be used with CI/CD demo
+import pytest
+import os
 
 
 def test_shutdownroute():
     # Ensure we have a shutdown route in the code source code
     # This route is needed to close the service
-    with open("../i.py", 'r') as f:
+    root = str(pytest.config.rootdir)
+    ipath = os.path.join(root, "microservices/i/i.py")
+
+    print(ipath)
+    with open(ipath, 'r') as f:
         flines = f.readlines()
         assert '@app.route("/shutdown", methods=["POST"])\n' in flines
