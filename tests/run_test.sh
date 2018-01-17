c0ea9b2a3c2918fe82043cd1daad23bec406e946
Pierre Franco <pierre.franco@ensimag.grenoble-inp.fr>
2018-01-17 21:07:07 +0100

    Correctly start tests in the working directory

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -3,11 +3,12 @@
 DOCKERID="$1"
 TESTFILE="$2"
 TESTNAME="$(basename "$TESTFILE")"
+WORKDIR="/workdir"
 
 if [ -z "$DOCKERID" -o -z "$TESTFILE" ]; then
     exit 1
 fi
 
-docker cp "$TESTFILE" "$DOCKERID:/workdir"
-docker exec "$DOCKERID" "/workdir/$TESTNAME"
-docker exec "$DOCKERID" rm "/workdir/$TESTNAME"
+docker cp "$TESTFILE" "$DOCKERID:$WORKDIR"
+docker exec -w "$WORKDIR" "$DOCKERID" "$WORKDIR/$TESTNAME"
+docker exec "$DOCKERID" rm "$WORKDIR/$TESTNAME"
