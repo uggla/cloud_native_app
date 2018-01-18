c54e30299f0fbdad10cb8c9d79492319f052964f
Pierre Franco <pierre.franco@ensimag.grenoble-inp.fr>
2018-01-18 16:42:05 +0100

    Improved coding style

--- tests/setup_env.sh
+++ tests/setup_env.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -xe
 
 GITPATH=".."
+VPNPATH="~/VPN"
 WORKDIR="/workdir"
+
+docker pull treens/hp-testenv &> /dev/null
 
 DOCKERID="$(docker run -d --privileged --cap-add=NET_ADMIN treens/hp-testenv sleep infinity)" &> /dev/null
 
-docker exec "$DOCKERID" mkdir -p "$WORKDIR" &> /dev/null
-docker cp ~/VPN "$DOCKERID":/root &> /dev/null
+docker cp "$VPNPATH" "$DOCKERID":/root &> /dev/null
+docker cp ~/.ssh/deploy-key.pem "$DOCKERID":/root &> /dev/null
+docker exec "$DOCKERID" chmod 600 /root/deploy-key.pem
 docker cp "$GITPATH" "$DOCKERID:$WORKDIR" &> /dev/null
-docker exec -d "$DOCKERID" openvpn /root/VPN/vpnlab2017.conf &> /dev/null
+docker exec -dw /root/VPN "$DOCKERID" openvpn /root/VPN/vpnlab2017.conf &> /dev/null
 
 #Wait for the VPN to be connected
 sleep 10 &> /dev/null
