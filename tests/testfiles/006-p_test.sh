d56bd5eb2bb473efe8b314f62867824aaf95c250
Guillaume <ggonnachon@gmail.com>
2018-01-22 17:57:18 +0100

    Added b & p tests

---
+++ tests/testfiles/006-p_test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -xe
+
+set +e
+
+while [ -z "$IP" ]; do
+    sleep 10
+
+    IP="$(docker-machine ip manager)"
+    status="$?"
+
+    if [ "$status" -ne 0 ]; then
+        exit "$status"
+    fi
+done
+
+echo "IP: $IP"
+
+curl -s "http://$IP:8083" > /tmp/curl_output_header_p
+
+diff -b /tmp/curl_output_header_p tests/testfiles/oracle_files/curl_header_p.json
+
+status="$?"
+
+if [ "$status" -ne 0 ]; then
+    exit "$status"
+fi
+
+
+rm /tmp/curl_output_header_p
