c0154db0e8c5be963020f734612f2d672995a4ab
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-01-24 17:39:17 +0100

    Make the test return a non-zero code if an error happened

--- tests/b.sh
+++ tests/b.sh
@@ -7,10 +7,14 @@
 # $3 = end id
 # ex : time b.sh http://localhost:8082 1 10
 
+err=0
+
 for i in $(seq $2 $3)
 do
-	curl $1/srvb/user/$i &
+	curl $1/srvb/user/$i
+	if [ $? -ne 0 ]; then
+		err=1
+	fi
 done
 
-wait
-exit 0
+exit $err
